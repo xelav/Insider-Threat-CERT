ffc4d0a195797cc7f7e934733e8111b4eefd601b
xelav <xelav.rus@gmail.com>
2020-05-02 16:11:09 +0300

    Refactor

--- src/cnn_trainer.py
+++ src/cnn_trainer.py
@@ -13,115 +13,129 @@ import wandb
 
 def prepare_batch(batch, device=None, non_blocking=None, num_classes=64):
 
-	actions = batch['actions']
-	actions = actions.to(device).to(torch.int64)
-	targets = batch['targets'].to(device).long()
-	# actions = F.one_hot(actions, num_classes=64).float()
-	return actions, targets
+    actions = batch['actions']
+    actions = actions.to(device).to(torch.int64)
+    targets = batch['targets'].to(device).long()
+    # actions = F.one_hot(actions, num_classes=64).float()
+    return actions, targets
 
 
-def create_supervised_trainer(model, optimizer, criterion, prepare_batch, metrics={},
-		device=None,
-		tqdm_log=False,
-	) -> Engine:
+def create_supervised_trainer(model,
+                              optimizer,
+                              criterion,
+                              prepare_batch,
+                              metrics={},
+                              device=None,
+                              tqdm_log=False,
+                              ) -> Engine:
 
-	def _update(engine, batch):
-		model.train()
-		optimizer.zero_grad()
+    def _update(engine, batch):
+        model.train()
+        optimizer.zero_grad()
 
-		actions, target = prepare_batch(batch, device=device)
+        actions, target = prepare_batch(batch, device=device)
 
-		scores = model(actions)
-		
-		loss = criterion(scores, target)
-		loss.backward()
-		optimizer.step()
+        scores = model(actions)
 
-		return {'loss': loss.item(), 'y_pred': scores, 'y_true': target}
+        loss = criterion(scores, target)
+        loss.backward()
+        optimizer.step()
 
-	model.to(device)
-	engine = Engine(_update)
+        return {'loss': loss.item(), 'y_pred': scores, 'y_true': target}
 
-	# Metrics
-	RunningAverage(output_transform=lambda x: x['loss']).attach(engine, 'running_average_loss')
-	Loss(
-		criterion, output_transform=lambda x: (x['y_pred'], x['y_true']),
-	).attach(engine, 'loss')
-	ROC_AUC(
-		output_transform=lambda x: (F.softmax(x['y_pred'], dim=1)[:,1], x['y_true'])
-	).attach(engine, 'roc_auc')
+    model.to(device)
+    engine = Engine(_update)
 
-	# TQDM
-	if tqdm_log:
-		pbar = ProgressBar(
-			persist=True,
-			bar_format='{desc}[{n_fmt}/{total_fmt}] {percentage:8.0f}%|{bar}{postfix} [{elapsed}<{remaining}]',
-		)
-		pbar.attach(engine, ['average_loss'])
+    # Metrics
+    RunningAverage(output_transform=lambda x: x['loss'])\
+        .attach(engine, 'running_average_loss')
+    Loss(
+        criterion, output_transform=lambda x: (x['y_pred'], x['y_true']),
+    ).attach(engine, 'loss')
+    ROC_AUC(
+        output_transform=lambda x:
+        (F.softmax(x['y_pred'], dim=1)[:, 1], x['y_true'])
+    ).attach(engine, 'roc_auc')
 
-	@engine.on(Events.EPOCH_COMPLETED)
-	def log_validation_results(engine):
-		metrics = engine.state.metrics
-		print(f"Epoch {engine.state.epoch} completed!")
-		print(f"{'Train Results':20} - Avg loss: {metrics['loss']:.6f}, ROC AUC: {metrics['roc_auc']:.6f}")
-		wandb.log({"train_loss": metrics['loss'], "train_roc_auc": metrics['roc_auc']}, commit=False)
+    # TQDM
+    if tqdm_log:
+        pbar = ProgressBar(
+            persist=True,
+        )
+        pbar.attach(engine, ['average_loss'])
 
-	return engine
+    @engine.on(Events.EPOCH_COMPLETED)
+    def log_validation_results(engine):
+        metrics = engine.state.metrics
+        print(f"Epoch {engine.state.epoch} completed!")
+        print(f"{'Train Results':20} - "
+              f"Avg loss: {metrics['loss']:.6f}, "
+              f"ROC AUC: {metrics['roc_auc']:.6f}")
+        wandb.log({
+            "train_loss": metrics['loss'],
+            "train_roc_auc": metrics['roc_auc']
+        }, commit=False)
+
+    return engine
 
 
 def create_supervised_evaluator(
-	model: torch.nn.Module,
-	prepare_batch,
-	criterion,
-	metrics = None,
-	device = None,
-	non_blocking: bool = False,
-	tqdm_log,
-	output_transform = lambda x, y, y_pred: (y_pred, y,),
-	checkpoint_dir='output/checkpoints/'
+        model: torch.nn.Module,
+        prepare_batch,
+        criterion,
+        metrics=None,
+        device=None,
+        non_blocking: bool = False,
+        tqdm_log,
+        output_transform=lambda x, y, y_pred: (y_pred, y,),
+        checkpoint_dir='output/checkpoints/'
 ) -> Engine:
 
-	if device:
-		model.to(device)
+    if device:
+        model.to(device)
 
-	def _inference(engine, batch):
-		model.eval()
-		with torch.no_grad():
-			actions, target = prepare_batch(batch, device=device, non_blocking=non_blocking)
-			scores = model(actions)
-			return (scores, target)
+    def _inference(engine, batch):
+        model.eval()
+        with torch.no_grad():
+            actions, target = prepare_batch(
+                batch, device=device, non_blocking=non_blocking)
+            scores = model(actions)
+            return (scores, target)
 
-	engine = Engine(_inference)
+    engine = Engine(_inference)
 
-	Loss(
-		criterion, output_transform=lambda x: x,
-	).attach(engine, 'loss')
-	ROC_AUC(
-		output_transform=lambda x: (F.softmax(x[0], dim=1)[:,1], x[1])
-	).attach(engine, 'roc_auc')
+    Loss(
+        criterion, output_transform=lambda x: x,
+    ).attach(engine, 'loss')
+    ROC_AUC(
+        output_transform=lambda x: (F.softmax(x[0], dim=1)[:, 1], x[1])
+    ).attach(engine, 'roc_auc')
 
+    if tqdm_log:
+        pbar = ProgressBar(persist=True)
+        pbar.attach(engine)
 
-	if tqdm_log:
-		pbar = ProgressBar(persist=True)
-		pbar.attach(engine)
+    # save the best model
+    # to_save = {'model': model}
+    # best_checkpoint_handler = Checkpoint(
+    # 	to_save,
+    # 	DiskSaver(checkpoint_dir, create_dir=True),
+    # 	n_saved=1,
+    # 	filename_prefix='best',
+    # 	score_function=lambda x: engine.state.metrics['roc_auc'],
+    # 	score_name="roc_auc",
+    # 	global_step_transform=lambda x, y : engine.train_epoch)
+    # engine.add_event_handler(Events.COMPLETED, best_checkpoint_handler)
 
-	# save the best model
-	# to_save = {'model': model}
-	# best_checkpoint_handler = Checkpoint(
-	# 	to_save,
-	# 	DiskSaver(checkpoint_dir, create_dir=True),
-	# 	n_saved=1, 
-	# 	filename_prefix='best',
-	# 	score_function=lambda x: engine.state.metrics['roc_auc'],
-	# 	score_name="roc_auc",
-	# 	global_step_transform=lambda x, y : engine.train_epoch)
-	# engine.add_event_handler(Events.COMPLETED, best_checkpoint_handler)
+    @engine.on(Events.COMPLETED)
+    def log_validation_results(engine):
+        metrics = engine.state.metrics
+        print(f"{'Validation Results':20} - "
+              f"Avg loss: {metrics['loss']:.6f}, "
+              f"ROC AUC: {metrics['roc_auc']:.6f}")
+        wandb.log({
+            "val_loss": metrics['loss'],
+            "val_roc_auc": metrics['roc_auc']
+        }, commit=True)
 
-	@engine.on(Events.COMPLETED)
-	def log_validation_results(engine):
-		metrics = engine.state.metrics
-		print(f"{'Validation Results':20} - Avg loss: {metrics['loss']:.6f}, ROC AUC: {metrics['roc_auc']:.6f}")
-		wandb.log({"val_loss": metrics['loss'], "val_roc_auc": metrics['roc_auc']}, commit=True)
-
-
-	return engine
+    return engine
--- src/dataset.py
+++ src/dataset.py
@@ -8,157 +8,182 @@ from torch.utils.data.sampler import SubsetRandomSampler
 
 from scipy.sparse import csc_matrix
 
+
 class CertDataset(Dataset):
 
-	@staticmethod
-	def pad_to_length(series, max_length=200, padding_id=0):
-		series = series.apply(lambda x: x[:max_length])
-		series = series.apply(lambda x: x + [padding_id] * (max_length - len(x)))
-		return series
+    @staticmethod
+    def pad_to_length(series, max_length=200, padding_id=0):
+        series = series.apply(lambda x: x[:max_length])
+        series = series.apply(
+            lambda x: x + [padding_id] * (max_length - len(x)))
+        return series
 
-	@staticmethod
-	def pad_topic_matricies(topic_matricies_array, max_length=200):
+    @staticmethod
+    def pad_topic_matricies(topic_matricies_array, max_length=200):
 
-		# FIXME: not sure if it's correct to do inplace operations inside of function
-		for idx, a in enumerate(topic_matricies_array):
-		    topic_matricies_array[idx] = csc_matrix(
-		    	(a.data, a.indices, a.indptr),
-		    	shape=(max_length, 100)
-		    	)
-		return topic_matricies_array
+        # FIXME: not sure if it's correct to do inplace operations inside of
+        # function
+        for idx, a in enumerate(topic_matricies_array):
+            topic_matricies_array[idx] = csc_matrix(
+                (a.data, a.indices, a.indptr),
+                shape=(max_length, 100)
+            )
+        return topic_matricies_array
 
-	@staticmethod
-	def prepare_dataset(pkl_file, answers_csv, min_length, max_length, padding_id=0, dataset_version='4.2'):
-		# TODO: drop weekends and holidays
+    @staticmethod
+    def prepare_dataset(
+            pkl_file,
+            answers_csv,
+            min_length,
+            max_length,
+            padding_id=0,
+            dataset_version='4.2'):
+        # TODO: drop weekends and holidays
 
-		df = pd.read_pickle(pkl_file)
-		df = df.reset_index().dropna()
+        df = pd.read_pickle(pkl_file)
+        df = df.reset_index().dropna()
 
-		main_df = pd.read_csv(answers_csv)
-		main_df = main_df[main_df['dataset'].astype(str) == str(dataset_version)]\
-			.drop(['dataset', 'details'], axis=1)
+        main_df = pd.read_csv(answers_csv)
+        main_df = main_df[main_df['dataset'].astype(str) == str(dataset_version)]\
+            .drop(['dataset', 'details'], axis=1)
 
-		main_df['start'] = pd.to_datetime(main_df['start'], format='%m/%d/%Y %H:%M:%S')
-		main_df['end'] = pd.to_datetime(main_df['end'], format='%m/%d/%Y %H:%M:%S')
+        main_df['start'] = pd.to_datetime(
+            main_df['start'], format='%m/%d/%Y %H:%M:%S')
+        main_df['end'] = pd.to_datetime(
+            main_df['end'], format='%m/%d/%Y %H:%M:%S')
 
-		df = df.merge(main_df, left_on='user', right_on='user', how='left')
-		df['malicious'] = (df.day >= df.start) & (df.day <= df.end)
-		df = df.drop(['start', 'end', 'day', 'user'], axis=1)
+        df = df.merge(main_df, left_on='user', right_on='user', how='left')
+        df['malicious'] = (df.day >= df.start) & (df.day <= df.end)
+        df = df.drop(['start', 'end', 'day', 'user'], axis=1)
 
-		df['action_length'] = df.action_id.apply(len)
-		df = df[df.action_length < min_length]
+        df['action_length'] = df.action_id.apply(len)
+        df = df[df.action_length < min_length]
 
-		df['action_id'] = CertDataset.pad_to_length(df.action_id, max_length=max_length, padding_id=padding_id)
+        df['action_id'] = CertDataset.pad_to_length(
+            df.action_id, max_length=max_length, padding_id=padding_id)
 
-		actions = np.vstack(df.action_id.values)
-		targets = df.malicious.values
+        actions = np.vstack(df.action_id.values)
+        targets = df.malicious.values
 
-		return actions, targets
+        return actions, targets
 
-	@staticmethod
-	def load_prepared_dataset(csv_file, min_length, max_length, remove_holidays, padding_id):
+    @staticmethod
+    def load_prepared_dataset(
+            csv_file,
+            min_length,
+            max_length,
+            remove_holidays,
+            padding_id):
 
-		df = pd.read_csv(csv_file)
-		df = df.drop(['user', 'date', ''])
+        df = pd.read_csv(csv_file)
+        df = df.drop(['user', 'date', ''])
 
-		df = df[df.action_length < min_length]
+        df = df[df.action_length < min_length]
 
-		df['action_id'] = CertDataset.pad_to_length(df.action_id, max_length=max_length, padding_id=padding_id)
+        df['action_id'] = CertDataset.pad_to_length(
+            df.action_id, max_length=max_length, padding_id=padding_id)
 
-		return df
+        return df
 
-	def __init__(self, actions, targets, content_topics=None, transform=None):
+    def __init__(self, actions, targets, content_topics=None, transform=None):
 
-		self.actions = actions
-		self.targets = targets.astype(int)
-		self.transform = transform
-		self.content_topics = content_topics
-		
+        self.actions = actions
+        self.targets = targets.astype(int)
+        self.transform = transform
+        self.content_topics = content_topics
 
-	def __len__(self):
-		return len(self.actions)
+    def __len__(self):
+        return len(self.actions)
 
-	def __getitem__(self, idx):
+    def __getitem__(self, idx):
 
-		if torch.is_tensor(idx):
-			idx = idx.tolist()
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
 
-		sample = {'actions': self.actions[idx], 'targets': self.targets[idx]}
+        sample = {'actions': self.actions[idx], 'targets': self.targets[idx]}
 
-		if self.content_topics is not None:
-			sample['content_topics'] = self.content_topics[idx].toarray()
+        if self.content_topics is not None:
+            sample['content_topics'] = self.content_topics[idx].toarray()
 
-		if self.transform:
-			sample = self.transform(sample)
+        if self.transform:
+            sample = self.transform(sample)
 
-		return sample
+        return sample
 
 
-def create_data_loaders(dataset, shuffle_dataset=True, validation_split=0.3, batch_size=16, random_seed=0):
+def create_data_loaders(
+        dataset,
+        shuffle_dataset=True,
+        validation_split=0.3,
+        batch_size=16,
+        random_seed=0):
 
-	# Creating data indices for training and validation splits:
-	dataset_size = len(dataset)
-	indices = list(range(dataset_size))
-	split = int(np.floor(validation_split * dataset_size))
-	if shuffle_dataset:
-		np.random.seed(random_seed)
-		np.random.shuffle(indices)
-	train_indices, val_indices = indices[split:], indices[:split]
+    # Creating data indices for training and validation splits:
+    dataset_size = len(dataset)
+    indices = list(range(dataset_size))
+    split = int(np.floor(validation_split * dataset_size))
+    if shuffle_dataset:
+        np.random.seed(random_seed)
+        np.random.shuffle(indices)
+    train_indices, val_indices = indices[split:], indices[:split]
 
-	# Creating PT data samplers and loaders:
-	train_sampler = SubsetRandomSampler(train_indices)
-	valid_sampler = SubsetRandomSampler(val_indices)
+    # Creating PT data samplers and loaders:
+    train_sampler = SubsetRandomSampler(train_indices)
+    valid_sampler = SubsetRandomSampler(val_indices)
 
-	train_loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, 
-											   sampler=train_sampler)
-	validation_loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size,
-													sampler=valid_sampler)
-	
-	return train_loader, validation_loader
+    train_loader = torch.utils.data.DataLoader(dataset,
+                                               batch_size=batch_size,
+                                               sampler=train_sampler)
+    validation_loader = torch.utils.data.DataLoader(dataset,
+                                                    batch_size=batch_size,
+                                                    sampler=valid_sampler)
+
+    return train_loader, validation_loader
+
 
 class SkipGramDataset(Dataset):
 
-	@staticmethod
-	def prepare_dataset(pkl_file, min_length, dataset_version='4.2'):
-		"""
-		Returns simple flat list of action ids
-		"""
+    @staticmethod
+    def prepare_dataset(pkl_file, min_length, dataset_version='4.2'):
+        """
+        Returns simple flat list of action ids
+        """
 
-		df = pd.read_pickle(pkl_file)
-		df = df.reset_index().dropna()
-		df = df.sort_values(['user', 'day'])
+        df = pd.read_pickle(pkl_file)
+        df = df.reset_index().dropna()
+        df = df.sort_values(['user', 'day'])
 
-		# filter out small sequences
-		df['action_length'] = df.action_id.apply(len)
-		df = df[df.action_length < min_length]
+        # filter out small sequences
+        df['action_length'] = df.action_id.apply(len)
+        df = df[df.action_length < min_length]
 
-		actions = df['action_id'].values.tolist()
-		# flatten list of lists
-		actions = list(itertools.chain.from_iterable(actions))
+        actions = df['action_id'].values.tolist()
+        # flatten list of lists
+        actions = list(itertools.chain.from_iterable(actions))
 
-		return actions
-		
+        return actions
 
-	def __init__(self, actions, window_size=3, padding_id=0, transform=None):
+    def __init__(self, actions, window_size=3, padding_id=0, transform=None):
 
-		self.actions = actions
-		self.transform = transform
-		self.window_size = window_size
-		self.padding_id = padding_id
+        self.actions = actions
+        self.transform = transform
+        self.window_size = window_size
+        self.padding_id = padding_id
 
-		padding_tail = [padding_id] * window_size
-		self.actions = padding_tail + self.actions + padding_tail * 2
+        padding_tail = [padding_id] * window_size
+        self.actions = padding_tail + self.actions + padding_tail * 2
 
-	def __len__(self):
-		return len(self.actions) - self.window_size * 3
+    def __len__(self):
+        return len(self.actions) - self.window_size * 3
 
-	def __getitem__(self, idx):
+    def __getitem__(self, idx):
 
-		if torch.is_tensor(idx):
-			idx = idx.tolist()
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
 
-		contexts = np.array(self.actions[idx:idx+2*self.window_size])
-		centers = contexts[self.window_size] # get the center of each window
-		contexts = np.delete(contexts, self.window_size) # remove center indecies
-		
-		return {'context': contexts, 'center': centers}
+        contexts = np.array(self.actions[idx:idx + 2 * self.window_size])
+        centers = contexts[self.window_size]  # get the center of each window
+        # remove center indecies
+        contexts = np.delete(contexts, self.window_size)
+
+        return {'context': contexts, 'center': centers}
